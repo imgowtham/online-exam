4bca0067a2e867b2a8d4d8b3558406734f8c8bfb
Gowtham <gowtham.079@gmail.com>
2021-06-24 23:37:20 +0530

    Navbar Committed

--- src/Components/Nav-Bar/nav-bar.components.jsx
+++ src/Components/Nav-Bar/nav-bar.components.jsx
@@ -16,7 +16,6 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
